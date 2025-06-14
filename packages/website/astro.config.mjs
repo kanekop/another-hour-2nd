d3a9eeceb763d4b9e8e9e08884a7bd8bac941145
kanekop <36024074+kanekop@users.noreply.github.com>
2025-06-14 20:11:44 +0900

    Update astro.config.mjs

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,18 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import i18n from 'astro-i18n';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    tailwind(),
-    i18n({
-      defaultLocale: 'ja',
-      locales: ['ja', 'en'],
-      routing: {
-        prefixDefaultLocale: false,
-      },
-    }),
-  ],
-});
+export default defineConfig({});
